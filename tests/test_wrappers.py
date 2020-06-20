ca8b61226c553866b7055c7df88edd0365dd776d
Tomas Nordin <tomasn@posteo.net>
2020-06-20 11:15:05 +0200

    Add a more recent data file for testing

--- tests/test_wrappers.py
+++ tests/test_wrappers.py
@@ -21,6 +21,7 @@ except EnvironmentError as e:
     sys.exit(1)
 
 DATAFILE1 = os.path.join(here, 'Example_Drive01.d7d')
+DATAFILE2 = os.path.join(here, 'Test2.dxd')
 
 if not os.path.exists(DATAFILE1):
     with gzip.open(DATAFILE1 + '.gz') as fi, open(DATAFILE1, 'wb') as fo:
@@ -36,6 +37,113 @@ DATAFILE1RECORDS_CH0 = {
            83.04696679115295, 82.59994506835938),
     95.5: (-0.08557390538044274, -0.09765923023223877,
            -0.06714072078466415, 0.0858997106552124)}
+
+DATAFILE2RECORDS_CH1 = {
+    0.0: (0.0, 0.0, 0.0, 0.0),
+    1.95: (0.0, 0.0, 0.0, 0.0),
+    4.4: (0.0, 0.0, 0.0, 0.0)}
+
+
+class TestWrappersTestfile2(unittest.TestCase):
+
+    def setUp(self):
+        self.initresult = wrappers.init()
+        if self.initresult != 0:
+            self.stop()
+        self.dwfileinfo = wrappers.open_data_file(DATAFILE2)
+
+    def test_initresult(self):
+        self.assertEqual(self.initresult, 0)
+
+    def test_open_data_file_result(self):
+        self.assertTrue(isinstance(self.dwfileinfo, wrappers.FileInfo))
+
+    def test_fileinfo_members(self):
+        self.assertEqual(self.dwfileinfo.sample_rate, 20000.0)
+        # assertAlmostEqual is also available
+        self.assertEqual(self.dwfileinfo.start_store_time, 43998.6823785301)
+        self.assertEqual(self.dwfileinfo.duration, 4.441)
+
+    def test_get_version(self):
+        version = wrappers.get_version()
+        self.assertGreater(version, 0)
+        self.assertIsInstance(version, int)
+
+    def test_get_channel_list_count(self):
+        self.assertEqual(wrappers.get_channel_list_count(), 2)
+
+    def test_get_channel_list(self):
+        channels = wrappers.get_channel_list()
+        self.assertEqual(channels[0],
+                         wrappers.Channel(index=0, name=b'Counting', unit=b'',
+                                          description=b'Rainflow',
+                                          color=16711680, array_size=20,
+                                          data_type=5))
+        self.assertEqual(channels[1],
+                         wrappers.Channel(index=1, name=b'Peak/Valley',
+                                          unit=b'', description=b'',
+                                          color=16711680, array_size=1,
+                                          data_type=5))
+
+    def test_get_reduced_values_count(self):
+        self.assertEqual(wrappers.get_reduced_values_count(0), (0, 0.05))
+        self.assertEqual(wrappers.get_reduced_values_count(1), (89, 0.05))
+
+    def test_get_reduced_values(self):
+        # (time_stamp, ave, min, max, rms)
+        records = wrappers.get_reduced_values(1, 0, 89)
+        recdict = {record[0]: record[1:] for record in records}
+        for timestamp, values in DATAFILE2RECORDS_CH1.items():
+            for recvalue, should in zip_longest(recdict[timestamp], values):
+                self.assertEqual(recvalue, should)
+
+    def test_channel_reduced_time_stamps_by_index(self):
+        #      0        1    2    3    4
+        # (time_stamp, ave, min, max, rms)
+        # Channel(index=1, name=b'Peak/Valley', ...
+        times = wrappers.channel_reduced(1, 0)
+        self.assertEqual(min(times), 0)
+        self.assertEqual(max(times), 4.4)
+        for previous, current in zip(times, times[1:]):
+            self.assertAlmostEqual(current - previous, 0.05)
+
+    def test_channel_reduced_time_stamps_by_name(self):
+        #      0        1    2    3    4
+        # (time_stamp, ave, min, max, rms)
+        # Channel(index=1, name=b'Peak/Valley', ...
+        times = wrappers.channel_reduced('Peak/Valley', 0)
+        self.assertEqual(min(times), 0)
+        self.assertEqual(max(times), 4.4)
+        for previous, current in zip(times, times[1:]):
+            self.assertAlmostEqual(current - previous, 0.05)
+
+    def test_channel_reduced_ave_by_name(self):
+        #      0        1    2    3    4
+        # (time_stamp, ave, min, max, rms)
+        # Channel(index=1, name=b'Peak/Valley', ...
+        averages = wrappers.channel_reduced('Peak/Valley', 1)
+        # first and last time_stamp in DATAFILE2RECORDS_CH1 represent
+        # the first and last record in channel 0
+        self.assertEqual(DATAFILE2RECORDS_CH1[0.0][0], averages[0])
+        self.assertEqual(DATAFILE2RECORDS_CH1[4.4][0], averages[-1])
+
+    def test_channel_reduced_ave_by_name_ascii(self):
+        #      0        1    2    3    4
+        # (time_stamp, ave, min, max, rms)
+        # Channel(index=1, name=b'Peak/Valley', ...
+        averages = wrappers.channel_reduced('Peak/Valley', 1, encoding='ascii')
+        # first and last time_stamp in DATAFILE2RECORDS_CH1 represent
+        # the first and last record in channel 0
+        self.assertEqual(DATAFILE2RECORDS_CH1[0.0][0], averages[0])
+        self.assertEqual(DATAFILE2RECORDS_CH1[4.4][0], averages[-1])
+
+    def tearDown(self):
+        result = wrappers.close_data_file()
+        if result:
+            print('error: close_data_file() returned', result)
+        result = wrappers.de_init()
+        if result:
+            print('error: de_init() returned', result)
 
 
 class TestWrappersExampleFile01(unittest.TestCase):
