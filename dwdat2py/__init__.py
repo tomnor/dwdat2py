f1628c4e644c77d102022607d82828b359a109d8
Tomas Nordin <tomasn@posteo.net>
2021-03-18 21:39:33 +0100

    Bump copyright year

--- dwdat2py/__init__.py
+++ dwdat2py/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2017, 2020 Tomas Nordin
+# Copyright 2017, 2020-2021 Tomas Nordin
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
